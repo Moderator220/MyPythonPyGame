d0d4de766c600cc39136d87de6dfbf5bd4e047e2
Vlad21Islav <vmitk@mail.ru>
2023-07-21 11:10:40 +0300

    словвввввввввввво

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ mainLoop = True
 while mainLoop:
     screen.fill('blue')
 
+    # sqr:
+
     widght, hight = pygame.display.get_surface().get_size()
     pygame.draw.rect(screen, 'green', 
                  (widght / 3, hight / 3, widght / 3, hight / 3))
 
-    
+    # font:
+
+    label = pygame.font.Font('font.ttf', int(hight / 3))
+
+    next_label = label.render('привет', True, 'red')
+
+    next_label_x = widght / 3
+    next_label_y = hight / 3
+    next_label_rect = next_label.get_rect(topleft=(next_label_x, next_label_y))
+
+    # blit font:
+
+    screen.blit(next_label, next_label_rect)
 
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
